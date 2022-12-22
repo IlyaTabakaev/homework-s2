feb5eabfe232d21d0732306246fb72829023f061
Ilya <osetia9595@gmail.com>
2022-12-22 21:29:46 +0300

    tsk1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("input three-digit number");
+int value = int.Parse(Console.ReadLine());
+
+
+
+ int a = value / 100;
+ int b = (value / 10) % 10;
+ int c = value % 10;
+
+Console.Write("second number is ");
+Console.WriteLine(b);
+
+
+
