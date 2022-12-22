731cbac450a9a5843af91cc20b4371ecf9f919f4
Ilya <osetia9595@gmail.com>
2022-12-22 21:55:17 +0300

    tsk3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Input daynumber: ");
+int i = int.Parse(Console.ReadLine());
+
+switch (i)
+{
+    case 1: Console.WriteLine("it's not a weekend"); break;
+    case 2: Console.WriteLine("it's not a weekend"); break;
+    case 3: Console.WriteLine("it's not a weekend"); break;
+    case 4: Console.WriteLine("it's not a weekend"); break;
+    case 5: Console.WriteLine("it's not a weekend"); break;
+    case 6: Console.WriteLine("WEEKEND"); break;
+    case 7: Console.WriteLine("WEEKEND"); break;
+    default: Console.WriteLine("It's not a day"); break;
+}
