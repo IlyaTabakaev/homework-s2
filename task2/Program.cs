dc593c5b4d176388dd3aa00b953bd54a1a9c28a5
Ilya <osetia9595@gmail.com>
2022-12-22 21:45:52 +0300

    tsk2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("input three-digit number");
+int value = int.Parse(Console.ReadLine());
+
+
+
+ int a = value / 100;
+ int b = (value / 10) % 10;
+ int c = value % 10;
+
+ if (value < 100)
+ {
+     Console.WriteLine("there is no third number");
+ }
+
+ else
+
+ {
+    Console.Write("third nimber is ");
+    Console.WriteLine(c);
+ }
+
+ 
